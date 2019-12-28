8d83818dffc0a20fcb7977a1b297ace60c90f9fb
BaptisteHiggs <baptistehiggs@gmail.com>
2019-12-29 00:10:04 +1100

    First final rework version. Just need to test it to see if it works (untested atm!)

--- webapprework.py
+++ webapprework.py
@@ -22,8 +22,8 @@ class Machine:
     def addBottleSeconds(self, bottle, seconds):
         """Adjust the end time for closing the pour for a bottle for a set amount of time in seconds
         
-        bottle should be an integer from 0-11 selecting the ingredient bottle
-        seconds should be a float of the amount of seconds to pour
+        'bottle' should be an integer from 0-11 selecting the ingredient bottle
+        'seconds' should be a float of the amount of seconds to pour
         """
         self.pourEndTimes[bottle] = time() + seconds
         if self.pourEndTimes[bottle] > self.workingTillTime:
@@ -46,60 +46,81 @@ class Machine:
         
 
     def checkBottlesForClosing(self):
-        """Check if any bottles should be closed due to time lapsed. Close them if appropriate"""
+        """Check if any bottles should be closed due to time lapsed. Close them if appropriate.
+        
+        Return True if not finished, return False if finished."""
+
+        stillGoing = True
+
         for bottle in self.bottles:
             if self.checkBottleOpen(bottle):
+                stillGoing = False
                 if self.pourEndTimes[bottle] < time():
                     self.closeBottle(bottle)
+        
+        return stillGoing
+
+    def startRecipe(self, recipe):
+        """Start pouring drinks appropriately for the given recipe.
+        
+        It should open the appropriate bottles, then set the times they should close.
+        'recipe' should be a length-12 list of floats denoting the amount of seconds necessary per bottle.
+        """
+        for bottle in self.bottles:
+            pourTime = recipe[bottle]
+            if pourTime > 0:
+                self.addBottleSeconds(bottle, pourTime)
+                self.openBottle(bottle)
 
 
+# Initialising the machine class instance
 machine = Machine()
 
+# Writing down the recipes (in seconds per bottle)
+recipes = {"1": [1,2,3,4,5,6,7,8,9,10,11,12],
+           "2": [1,1,1,1,1,1,3,3,3,3,3,3],
+           "3": [0,0,0,0,0,0,0,0,0,0,2,2],
+           "4": [1,2,3,1,2,3,1,2,3,0,0,4.0243]}
 
+# Initialising Flask
 app = Flask(__name__)
-print("1")
-workingTillTime = 0
-drinkLength = 5000
-currentDrink = ""
 
-
-# GPIO.output(pin[counter], True)        <- Start specific ingredient flow
-# sleep(recipe[currentDrink][counter])   <- Wait x seconds
-# GPIO.output(pin[counter], False)       <- Stop flow
-
-
+# This is a decorator that makes the main() function a Flask function - it can run a website.
 @app.route('/', methods=['POST', 'GET'])
-def main():
-	"""try:
-		_ = workingTillTime
-	except UnboundLocalError:
-		print("updating variables")
-		workingTillTime = 0
-		drinkLength = 5
-	"""
+def main(): # This is what's called when the page is searched for
+    
+    # This checks for information coming from the page in the form of a post request.
+    if request.method == 'POST':
+        
+        # Finding the times both this script and the web page are reporting
+        pythonTime = round(time()*1000)
+        javascriptTime = int(str(request.data)[2:-1].split(",")[1])
 
-	if request.method == 'POST':
-		
-		pythonTime = round(time()*1000)
-		javascriptTime = int(str(request.data)[2:-1].split(",")[1])
-		drinkID = str(str(request.data)[2:-1].split(",")[0])
+        # Getting the info of what drink the user wants!
+        drinkID = str(str(request.data)[2:-1].split(",")[0])
 
-		# Debugging time print statements
-		print("Req Data: " + str(request.data)[2:-1])
-		print("js: " + str(javascriptTime) + " py: " + str(pythonTime))
-		print("Difference (js - py) = " + str(javascriptTime - pythonTime))
-		
-		if (javascriptTime > workingTillTime):
-			print("Let's mix up drink:")
-			print(drinkID)
-			currentDrink = drinkID
-			workingTillTime = javascriptTime + drinkTime(currentDrink)
-			makeDrink(drinkID)
-		else:
-			print("Currently mixing drink " + str(currentDrink) + "... " + str(round(workingTillTime - javascriptTime, 2)) + " milliseconds left!")			
-	return render_template('index.html')
+        # Printing the received data
+        print("Req Data: " + str(request.data)[2:-1])
 
-if __name__	== "__main__":
-	app.run(debug=True, host="0.0.0.0", port=80)
+        # Printing out both this script's and the webpage's reported times
+        print("py: " + str(pythonTime) + " js: " + str(javascriptTime))
+        print("Difference (py - js) = " + str(pythonTime - javascriptTime))
+
+        if (javascriptTime > machine.workingTillTime):
+            print("Let's mix up a drink: ")
+            print(drinkID)
+            machine.startRecipe(recipes[drinkID])
+
+            # This continuously checks whether bottles should be closed, and returns false if they're all closed.
+            while machine.checkBottlesForClosing():
+                # Sleep a little to allow the pi to do things other than a really fast loop.
+                # I think not doing this is what caused an earlier version to crash.
+                sleep(0.05)
+        else:
+            print("Currently mixing a drink! ")            
+    return render_template('index.html')
+
+if __name__    == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=80)
 
 #If stuck in a stupid loop of not working type in localhost:80/index.html and it won't work but it seems to reset it somewhat
